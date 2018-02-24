787d6ed0b108a89e0611f6dba8549cb8fae2eac6
Arthur Diniz <arthurbdiniz@gmail.com>
2018-02-24 01:58:52 -0300

    Clean script and code from reset_password
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- js/main.js
+++ js/main.js
@@ -140,6 +140,34 @@ function toggleAddDialogWatchVideo(visible, videoUrl) {
 };
 
 
+
+//----------------RESET PASSWORD---------------------- //
+
+function sendPasswordReset() {
+  var email = document.getElementById('email').value;
+  // [START sendpasswordemail]
+  firebase.auth().sendPasswordResetEmail(email)
+  .then(function() {
+
+    alert('Password Reset Email Sent!');
+    goToSignIn()
+
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    if (errorCode == 'auth/invalid-email') {
+      alert(errorMessage);
+    } else if (errorCode == 'auth/user-not-found') {
+      alert(errorMessage);
+    }
+    console.log(error);
+
+  });
+
+}
+
 //----------------SIGNIN---------------------- //
 
 function handleSignIn() {
