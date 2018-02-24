1171cbb4facd288873c886982af15cc0702cd048
Arthur Diniz <arthurbdiniz@gmail.com>
2018-02-24 01:52:41 -0300

    Clean script and code from sign_in
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- js/main.js
+++ js/main.js
@@ -140,14 +140,48 @@ function toggleAddDialogWatchVideo(visible, videoUrl) {
 };
 
 
+//----------------SIGNIN---------------------- //
 
+function handleSignIn() {
+  if (firebase.auth().currentUser) {
+    // [START signout]
+    firebase.auth().signOut();
+    // [END signout]
+  } else {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        goToMain() //After successful login, user will be redirected to home.html
+      }
+    });
+  }
+}
 
 //----------------SIGNUP---------------------- //
-
-document.getElementById('buttonRegister').addEventListener('click', function() {
-  handleSignUp()
-});
-
 
 function handleSignUp() {
   var coupon = document.getElementById('coupon').value;
